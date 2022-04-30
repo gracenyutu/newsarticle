06df5ab9335a3c7b6460358f111f55d74f877cd9
Grace Nyutu <graceynyutu@gmail.com>
2022-05-01 00:06:49 +0300

    define News class and pass in parameters needed in the news objects

---
+++ app/models/news.py
@@ -0,0 +1,14 @@
+class News:
+    '''
+    News class to define News Objects
+    '''
+
+    def __init__(self,id,name,author,title,description,urlToImage,content):
+        self.id =id
+        self.name = name
+        self.author = author
+        self.title = title
+        self.description = description
+        self.urlToImage = urlToImage
+        self.content = content
+        
